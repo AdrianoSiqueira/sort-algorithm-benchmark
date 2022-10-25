29901de3a53e625f3188c79b502646164e736a19
adriano <as.adriano.siqueira@gmail.com>
2022-10-25 20:28:00 -0300

    Add call for the app window.

--- src/main/java/com/github/adrianosiqueira/sortalgorithmbenchmark/AppMainClass.java
+++ src/main/java/com/github/adrianosiqueira/sortalgorithmbenchmark/AppMainClass.java
@@ -1,6 +1,11 @@
 package com.github.adrianosiqueira.sortalgorithmbenchmark;
 
+import com.github.adrianosiqueira.sortalgorithmbenchmark.gui.window.AppWindow;
+import javafx.application.Application;
+
 public class AppMainClass {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Application.launch(AppWindow.class);
+    }
 }
